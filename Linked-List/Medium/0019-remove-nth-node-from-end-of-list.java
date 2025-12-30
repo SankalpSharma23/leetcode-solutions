53acbcdef536dbac84ead9754b6b6175536ab308
Sankalp Sharma <sankalpsharma232005@gmail.com>
2025-12-30 17:53:35 +0530

    🟡 Add: 19. 19. Remove Nth Node From End of List (java)

---
+++ Linked-List/Medium/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,22 @@
+// ═════════════════════════════════════════════════════════════════
+// 19. 19. Remove Nth Node From End of List
+// ═════════════════════════════════════════════════════════════════
+// Difficulty: Medium
+// Topics: Linked List, Two Pointers
+//
+// Acceptance Rate: 50.5%
+//
+// LeetCode: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+// Synced: 2025-12-30T12:23:35.158Z
+// ═════════════════════════════════════════════════════════════════
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
